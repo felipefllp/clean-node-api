8df9c2e374a750c7b6906bc0f3af3c23b2eb82fa
Felipe Lopes <felipefllp@msn.com>
2021-01-05 05:35:15 -0300

    test: ensure SignUoController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -6,8 +6,24 @@ describe('Signup Controller', () => {
     const sut = new SignupController()
     const httpRequest = {
       body: {
-        // name: 'any_name',
         email: 'any_email@mail.com',
+        password: 'any-password',
+        passwordConfirmation: 'any-password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).to.equal(400)
+    expect(httpResponse.body).to.be.an('error')
+  })
+})
+
+describe('Signup Controller', () => {
+  it('Should return 400 if no email is provided', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
         password: 'any-password',
         passwordConfirmation: 'any-password'
       }
